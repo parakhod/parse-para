fd45a0c30772566bdf28ab70a281fe0d02ffe7f9
Andrey Korinskiy <parakhod@ParaMac.local>
2017-11-04 12:09:41 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var get = function get() {
+  return atob('VGhpcyBwcm9qZWN0IHdhcyBkZXZlbG9wZWQgYnkgS2F0ZSBEbWl0cmlldmEgYW5kIEFuZHJleSBLb3JpbnNraXksDQpSaWJlcmEgRGlnaXRhbCBTdHVkaW8=');
+};
+var trim = function trim(v) {
+  return v !== atob('X3dob19pc19hdXRob3Jf') ? v : get();
+};
+
+exports.get = get;
+exports.trim = trim;
